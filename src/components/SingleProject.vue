03de71095f33ff740347bd0e3f503b0a21ae2cfc
zioan <zaharia.ioan@gmail.com>
2021-10-29 11:56:33 +0200

    init

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="showDetails">{{project.title}}</h3>
+    </div>
+    <div class="details" v-if="toggleDetails">
+      <p>{{project.details}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data() {
+    return {
+      toggleDetails: false,
+    }
+  },
+  methods: {
+    showDetails() {
+      this.toggleDetails = !this.toggleDetails
+    }
+  }
+
+}
+</script>
+
+<style>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+    border-left: 4px solid #e90074;
+  }
+  h3{
+    cursor: pointer;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project = "project"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleProject from "../components/SingleProject.vue"
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SingleProject
+  },
+  data(){
+    return {
+      projects: [],
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => this.projects = data)
+      .catch((err) => console.log(err.message))
   }
 }
 </script>
